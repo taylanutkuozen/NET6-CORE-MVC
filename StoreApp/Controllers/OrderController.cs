5ffc0e6f200697d5f0b34f9dd2884e01c7510c0c
taylanutkuozen <taylanutku.ozen@gmail.com>
2025-05-12 23:43:01 +0300

    Session Order Final

---
+++ StoreApp/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+using Entities.Models;
+namespace StoreApp.Controllers
+{
+    public class OrderController:Controller
+    {
+        private readonly IServiceManager _serviceManager;
+        private readonly Cart _cart;
+        public OrderController(IServiceManager serviceManager, Cart cart)
+        {
+            _serviceManager=serviceManager;
+            _cart=cart;
+        }
+        public ViewResult Checkout() => View(new Order());
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Sahteciliği onlemek amaciyla
+        public IActionResult Checkout([FromForm]Order order)
+        {
+            if(_cart.Lines.Count()==0)
+            {
+                ModelState.AddModelError("","Sorry your cart is empty");
+            }
+            if(ModelState.IsValid)
+            {
+                order.Lines=_cart.Lines.ToArray();
+                _serviceManager.OrderService.SaveOrder(order);
+                _cart.Clear();
+                return RedirectToPage("/Complete", new {OrderID=order.OrderID});
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
