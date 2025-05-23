85d238066fdbd05e0e3da8c56423a8cbc1a69ec1
taylanutkuozen <taylanutku.ozen@gmail.com>
2025-05-23 22:13:33 +0300

    ServiceExtension

---
+++ StoreApp/Infrastructure/Extensions/ServiceExtension.cs
@@ -0,0 +1,46 @@
+using Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using Repositories.Contracts;
+using Services.Contracts;
+using Services;
+using Entities.Models;
+using StoreApp.Models;
+namespace StoreApp.Infrastructure.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigureDbContext(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<RepositoryContext>(options=> //İhtiyaç olması durumunda Db kullanacağımızı belirtmiş olduk.
+            {
+                options.UseSqlite(configuration.GetConnectionString("sqlconnection"),b=>b.MigrationsAssembly("StoreApp"));//Migration hedefi belirttik.
+            });
+        }
+        public static void ConfigureSession(this IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();/*Configurasyon ifadeleri tanimladik*/
+            services.AddSession(options=>{
+                options.Cookie.Name="StoreApp.Session";
+                options.IdleTimeout=TimeSpan.FromMinutes(10);/*10 dakika sonra oturumu dusur.*/
+            });/*2.adim Middleware insasi buradan devam eder.*/
+            services.AddSingleton<IHttpContextAccessor,HttpContextAccessor>();
+            /*Her user bu nesneyi ayri ayri uretmesine gerek yok, bir defa uretildikten sonra herkes bu nesneyi kullanabilir. O yuzden Singleton kullandik.*/
+            services.AddScoped<Cart>(c=> SessionCart.GetCart(c));
+        }
+        public static void ConfigureRepositoryRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager,RepositoryManager>();
+            services.AddScoped<IProductRepository,ProductRepository>();
+            services.AddScoped<ICategoryRepository,CategoryRepository>();
+            services.AddScoped<IOrderRepository,OrderRepository>();
+        }
+        public static void ConfigureServicesRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IServiceManager, ServiceManager>();
+            services.AddScoped<IProductService, ProductManager>();
+            services.AddScoped<ICategoryService,CategoryManager>();
+            services.AddScoped<IOrderService,OrderManager>();
+        }
+    }
+}
--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -1,39 +1,15 @@
 using Microsoft.EntityFrameworkCore;
-using Repositories;
-using Entities.Models;
-using Repositories.Contracts;
 using System.Net;
-using Services.Contracts;
-using Services;
 using AutoMapper;
 using System.Reflection;
-using StoreApp.Models;
+using StoreApp.Infrastructure.Extensions;
 var builder = WebApplication.CreateBuilder(args);//Uygulama başlayacağı ifade edilmiş. Servis yok. hiçbir şey yok
 builder.Services.AddControllersWithViews();//Controller kullanacagim ve View nesnelerinden istifade edecegim.
 builder.Services.AddRazorPages();//Razor Page kullanabilmek için Controller kullanmaya gerek olmayacaktir.
-builder.Services.AddDbContext<RepositoryContext>(options=> //İhtiyaç olması durumunda Db kullanacağımızı belirtmiş olduk.
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),b=>b.MigrationsAssembly("StoreApp"));//Migration hedefi belirttik.
-});
-builder.Services.AddDistributedMemoryCache();/*Session Middleware insasi icin kullanilacaktir.On bellek sagliyor. Server tarafinda bilgileri tutuyor.*/
-builder.Services.AddSession(options=>
-{
-    /*Configurasyon ifadeleri tanimladik*/
-    options.Cookie.Name="StoreApp.Session";
-    options.IdleTimeout=TimeSpan.FromMinutes(10); /*10 dakika sonra oturumu dusur.*/
-}
-);/*2.adim Middleware insasi buradan devam eder.*/
-builder.Services.AddSingleton<IHttpContextAccessor,HttpContextAccessor>();/*
-Her user bu nesneyi ayri ayri uretmesine gerek yok, bir defa uretildikten sonra herkes bu nesneyi kullanabilir. O yuzden Singleton kullandik.*/
-builder.Services.AddScoped<IRepositoryManager,RepositoryManager>();
-builder.Services.AddScoped<IProductRepository,ProductRepository>();
-builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
-builder.Services.AddScoped<IOrderRepository,OrderRepository>();
-builder.Services.AddScoped<IServiceManager, ServiceManager>();
-builder.Services.AddScoped<IProductService, ProductManager>();
-builder.Services.AddScoped<ICategoryService,CategoryManager>();
-builder.Services.AddScoped<IOrderService,OrderManager>();
-builder.Services.AddScoped<Cart>(c=> SessionCart.GetCart(c));
+builder.Services.ConfigureDbContext(builder.Configuration);
+builder.Services.ConfigureSession();
+builder.Services.ConfigureRepositoryRegistration();
+builder.Services.ConfigureServicesRegistration();
 //builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 var app = builder.Build();  //Uygulamanın inşası var denilmiş.
